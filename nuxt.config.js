d85a53ac733e5253680f84c2894f2047050ab0c2
dethMastery <owner@detzz.in.th>
2021-12-28 21:40:01 +0700

    retitle

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Just Some of New Year Celebrate',
+    title: 'Just Some of New Year Celebration',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
