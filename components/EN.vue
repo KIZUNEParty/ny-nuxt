059b3df56669bd8510f72ba564be65f7fa63ffca
dethMastery <owner@detzz.in.th>
2021-12-29 21:18:06 +0700

    adding w text

--- components/EN.vue
+++ components/EN.vue
@@ -3,5 +3,9 @@
         <span class="heading">
             Dear {{ $route.query.name }}
         </span>
+        <br>
+        <span id="wText">
+            &nbsp;&nbsp;&nbsp;&nbsp;คนที่ได้อ่านข้อความนี้ ถึงแม้ว่าจะช้าไปหน่อยที่ผมจะมา Merry Christmas ทุกคน แต่ก็สุขสันต์วันปีใหม่นะครับ ขอให้มีความสุขในปี {{ fullYear }} ที่กำลังจะมาถึงนี้นะครับ ขอให้เวลาทำอะไรๆ ก็ดีไปหมดทุกอย่างเลยยย แล้วก็ขอให้รอดจากไอประเทศเส็งเคร็งนี่นะครับ สวัสดีครับ <i class="em em-wink"></i> <i class="em em-smiley"></i>
+        </span>
     </div>
 </template>
