ff3896058c0a02b4b98b2eb13062e71f77e8a397
dethMastery <owner@detzz.in.th>
2021-12-31 22:34:28 +0700

    adding text container

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="foot">
-        &copy; 2021 <a href="https://detzz.in.th">Suphakit P.</a> All Right Reserve
+    <div class="clover">
+        <div class="foot">
+            <span class="f-text">
+                &copy; 2021 <a href="https://detzz.in.th">Suphakit P.</a> All Right Reserve
+            </span>
+        </div>
     </div>
 </template>
